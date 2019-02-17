d500ab6e4922f83775b6ceb5e2f8ec232dfea4f2
deasway <alanli9898@gmail.com>
2019-02-16 20:16:16 -0500

    formatted web of science data

--- scripts/WebOfScienceScraper.py
+++ scripts/WebOfScienceScraper.py
@@ -33,14 +33,14 @@ password = "---"
 
 def scraperMain(browser):
 
-    username_field = browser.find_element_by_id("username")
-    password_field = browser.find_element_by_id("password")
-    submit_button = browser.find_element_by_name("submit")
+    # username_field = browser.find_element_by_id("username")
+    # password_field = browser.find_element_by_id("password")
+    # submit_button = browser.find_element_by_name("submit")
 
-    username_field.send_keys(username)
-    password_field.send_keys(password)
-    submit_button.click()
-    
+    # username_field.send_keys(username)
+    # password_field.send_keys(password)
+    # submit_button.click()
+
     #TODO: Automate 2FA
     #browser.switch_to.default_content()
     #iframe = browser.find_element_by_id("duo_iframe")
@@ -49,13 +49,13 @@ def scraperMain(browser):
     #tfa_button = browser.find_element_by_class_name("row-label")
     #tfa_button.click()
 
-    print("Manually do 2FA.")
-    wait = WebDriverWait(browser, 42)
-    temp = wait.until(EC.visibility_of_element_located((By.XPATH, "//*[@id=\"value(input1)\"]")))
-    print("WOS loaded.")
-    
+    # print("Manually do 2FA.")
+    # wait = WebDriverWait(browser, 42)
+    # temp = wait.until(EC.visibility_of_element_located((By.XPATH, "//*[@id=\"value(input1)\"]")))
+    # print("WOS loaded.")
+
     words_dict = open_csv("braveNewWords.csv")
-    
+
     try:
         #Words_correlated is a list filled with 5-tuples
         #The 5-tuple has the structure:
@@ -69,12 +69,12 @@ def scraperMain(browser):
             words_correlated.append(f_tuple)
             if hits:
                 print(f_tuple)
-            
+
             #return to main search screen
             browser.get(url)
             wait = WebDriverWait(browser, 42)
             temp = wait.until(EC.visibility_of_element_located((By.XPATH, "//*[@id=\"value(input1)\"]")))
-            
+
         browser.quit()
         return words_correlated
         #print(words_correlated)
@@ -91,33 +91,33 @@ def scraperMain(browser):
 def searchTerm(driver, string):
     #Returns (total # of hits, dict of {year, # in that year}, dict of {domain, # in that domain})
     #For a given word
-    
+
     search_bar = driver.find_element_by_xpath("//*[@id=\"value(input1)\"]")
     search_bar.click()
-    
+
     #clearing bar
     try:
         search_bar.clear()
     except:
         ActionChains(driver).key_down(Keys.CONTROL).send_keys('a').key_up(Keys.CONTROL).perform()
         search_bar.send_keys(Keys.DELETE)
-    
+
     search_bar.send_keys(string)
     search_bar.send_keys(Keys.ENTER)
     time.sleep(4)
-    
-    try:    
+
+    try:
         raw_hit_count_element = browser.find_element_by_id("hitCount.top")
         raw_hit_count = raw_hit_count_element.text
-        
+
         year_stats_link = driver.find_element_by_class_name("link-style1")
         year_stats_link.click()
         time.sleep(2)
-        
+
         year_stats_dict, domains_stats_dict = detailsHarvest(browser)
         #print(domains_stats_dict)
         return raw_hit_count, year_stats_dict, domains_stats_dict
-        
+
     except Exception as e:
         #TODO: Handle this case
         print("No/minimal stats on publication years for word " + string)
@@ -139,13 +139,13 @@ def yearSplitter(string):
     yearPattern = re.compile("[1,2][0-9][0-9][0-9]")
     yearMatch = yearPattern.search(string)
     year = str(yearMatch.group())
-    
+
     #this regex is adorable ---v
     numPattern = re.compile("\(.*\)")
     numMatch = numPattern.search(string)
     number = str(numMatch.group())
     number = re.sub("(\(|\)|,)", "", number)
-    
+
     return year, number
 
 def domainsRegex(string):
@@ -155,18 +155,18 @@ def domainsRegex(string):
         return domain.group()
     return None
     #TODO
-    
+
 def domainsSplitter(string):
     domainsPattern = re.compile("[A-Z ]* *\(")
     domainsMatch = domainsPattern.search(string)
     domain = str(domainsMatch.group())
     domain = re.sub(" \(", "", domain)
-    
+
     numPattern = re.compile("\(.*\)")
     numMatch = numPattern.search(string)
     number = str(numMatch.group())
     number = re.sub("(\(|\)|,)", "", number)
-    
+
     return domain, number
     #TODO
 
@@ -179,19 +179,19 @@ def detailsHarvest(driver):
     domain_stats_elements = driver.find_elements_by_class_name("ra-summary-text")
     year_stats_dict = {}
     domains_stats_dict = {}
-    
+
     for e in year_stats_elements:
         element_text = e.text
         if yearRegex(element_text):
             e1, e2 = yearSplitter(element_text)
             year_stats_dict[e1] = e2
-    
+
     for e in domain_stats_elements:
         element_text = e.text
         if domainsRegex(element_text):
             e1, e2 = domainsSplitter(element_text)
             domains_stats_dict[e1] = e2
-    
+
     return year_stats_dict, domains_stats_dict
 
 
@@ -211,7 +211,7 @@ def open_csv(file):
 # In[83]:
 
 
-browser = webdriver.Chrome()
+browser = webdriver.Chrome("/Users/alan/Documents/Brackets/scripts/chromedriver")
 browser.get(url)
 
 data_list = scraperMain(browser)
