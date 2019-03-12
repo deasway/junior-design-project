9897c81a86c0f4689de1211aa253289e7a678f76
Dasom <deom119@gmail.com>
2019-03-11 22:33:01 -0400

    search amp search alert

--- public/js/amplitude.js
+++ public/js/amplitude.js
@@ -1,38 +1,54 @@
-var dates = [];
-var year = 2000;
-dates.push(year);
-for (i = 0; i < 9; i++) {
-    year += 1;
-    dates.push(year);
-}
-
+var names = [];
 
 window.onload = function() {
-    var chart = c3.generate({
-    title: {
-        text: "Number of Occurrences for 'Cloning'",
-    },
-    data: {
-        x: 'x',
-        
-        columns: [
-            ['x'].concat(dates),
-            ['Number Of Occurrences', 30, 200, 100, 400, 150, 250, 70, 40, 60, 100],
-        ]
-    },
-    axis: {
-        x: {
-            type: 'date'
-        }
-    }
-        
+    var config = {
+        apiKey: "AIzaSyBXViFaFbggSb0QqB1QwmAtuE3XO545NF0",
+        authDomain: "junior-design-project.firebaseapp.com",
+        databaseURL: "https://junior-design-project.firebaseio.com",
+        projectId: "junior-design-project",
+        storageBucket: "junior-design-project.appspot.com",
+        messagingSenderId: "986723685667"
+    };
+    firebase.initializeApp(config);
+    var database = firebase.database();
+    database.ref('/').orderByChild('name').on("value", function(snapshot) {
+        snapshot.forEach(function(child) {
+            a = child.val()['name'];
+            dataPush(a);
+        });
+
     });
+
+
 
 };
 
+function dataPush(term) {
+    //names = [];
+    var a = "" + term;
+    names.push(a.toLowerCase());
+    names.sort();
+}
+
+$( function() {
+    names;
+    $( "#myInput" ).autocomplete({
+        source: names
+    });
+});
 
 
-
+function validate() {
+    names;
+    var inpObj = (document.getElementById('myInput').value);
+    var in_array = jQuery.inArray(inpObj.toLowerCase(), names);
+    if (in_array == -1) {
+        alert("No Result Found");
+        document.getElementById("alert").innerHTML = "<span class='error'>No Matching Terms Found. Please Search Again.</span>";
+        return false;
+    }
+    return true;
+}
 
 
 
